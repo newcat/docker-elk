ba3b93096c6e975ab7e50c1a9becf2ebb45d42bf
newcat <freddy.wagner@web.de>
2019-03-21 23:01:55 +0100

    Clearing at the start, more debug

--- iota/index.js
+++ iota/index.js
@@ -1,6 +1,9 @@
 const zmq = require("zeromq");
 const sock = zmq.socket("sub");
 
+const DEBUG = false;
+const CLEAR_AT_START = true;
+
 const { Client } = require('@elastic/elasticsearch')
 const client = new Client({ node: 'http://elasticsearch:9200',  })
 
@@ -20,25 +23,32 @@ function handleMessage(msg) {
                 address: data[2],
                 value: parseInt(data[3] || "0"),
                 obsoleteTag: data[4],
-                transactionTimestamp: data[5],
+                transactionTimestamp: new Date(parseInt(data[5]) * 1000),
                 currentIndex: parseInt(data[6] || "0"),
                 lastIndex: parseInt(data[7] || "0"),
                 bundle: data[8],
                 trunkTransaction: data[9],
                 branchTransaction: data[10],
-                received: parseInt(data[3] || "0"),
+                received: new Date(parseInt(data[11] || "0")),
                 tag: data[12]
             };
             break;
     }
 
+    if (dataObject) {
+        dataObject.timestamp = new Date();
+    }
+
     if (data[0] === "tx") {
-        client.index({
-            index: "iota_" + data[0],
-            id: data[1],
-            body: dataObject
-        });
-        console.log("Stored TX");
+        if (!DEBUG) {
+            client.index({
+                index: "iota_" + data[0],
+                id: data[1],
+                body: dataObject
+            });
+        } else {
+            console.log(dataObject);
+        }
     }
 
 };
@@ -49,20 +59,32 @@ function sleep(ms) {
     });
 }
 
-(async () => {
+if (!DEBUG) {
+    (async () => {
 
-    await sleep(30000);
+        await sleep(30000);
 
-    while (!(await client.ping())) {
-        await sleep(5000);
-    }
-
-    for (const c of subscribedChannels) {
-        if (!(await client.indices.exists({ index: "iota_" + c }))) {
-            await client.indices.create({ index: "iota_" + c });
+        while (!(await client.ping())) {
+            await sleep(5000);
         }
-    }
 
+        if (CLEAR_AT_START) {
+            console.log("Clearing all iota indices");
+            if (await client.indices.exists({ index: "iota_" + c })) {
+                await client.indices.delete({ index: "iota_" + c });
+            }
+        }
+
+        for (const c of subscribedChannels) {
+            if (!(await client.indices.exists({ index: "iota_" + c }))) {
+                console.log("Creating index iota_" + c);
+                await client.indices.create({ index: "iota_" + c });
+            }
+        }
+
+        sock.on('message', handleMessage);
+
+    })();
+} else {
     sock.on('message', handleMessage);
-
-})();
+}
